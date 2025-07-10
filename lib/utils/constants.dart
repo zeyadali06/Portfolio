bc7b398edfe13c7967c90ca1522e1209c2aa5878
Zeyad Ali <z01551153743@gmail.com>
2025-07-10 18:50:30 +0300

    add LinkAI app to projects

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 final class Constants {
-  static const String cvLink = "https://drive.google.com/file/d/18r9rWnw32geYrsxO8HUS0iJ8yvPldjVA/view?usp=sharing";
+  static const String cvLink = "https://drive.google.com/file/d/1Qfuqfv62nRu_JomYbyhHt72DuklVerGD/view?usp=sharing";
   static const double kDesktopHorizontalPadding = 80;
   static const double kDesktopVerticalPadding = 50;
   static const double kTabletHorizontalPadding = 40;
